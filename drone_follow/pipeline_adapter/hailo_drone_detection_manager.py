0ca1186726fcc056100aed34b2082173967788ee
Nadav h <nadavh@hailo.ai>
2026-03-02 15:11:31 +0200

    Fix params

--- drone_follow/pipeline_adapter/hailo_drone_detection_manager.py
+++ drone_follow/pipeline_adapter/hailo_drone_detection_manager.py
@@ -371,6 +371,8 @@ def create_app(shared_state, target_state=None, eos_reached=None, ui_state=None,
                 video_height=self.video_height,
                 frame_rate=self.frame_rate,
                 sync=self.sync,
+                mirror_image=self.options_menu.horizontal_mirror,
+                vertical_mirror=self.options_menu.vertical_mirror,
             )
 
             detection_pipeline = INFERENCE_PIPELINE(
