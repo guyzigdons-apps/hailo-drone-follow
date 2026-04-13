adf336a786673d11dfdc55f522d66549e4b04832
Nadav h <nadavh@hailo.ai>
2026-04-13 11:47:24 +0300

    Clean code

--- drone_follow/pipeline_adapter/hailo_drone_detection_manager.py
+++ drone_follow/pipeline_adapter/hailo_drone_detection_manager.py
@@ -681,7 +681,7 @@ def create_app(shared_state, target_state=None, eos_reached=None, ui_state=None,
                     video_height=self.video_height,
                     frame_rate=self.frame_rate,
                     sync=self.sync,
-                    mirror_image=False,
+                    horizontal_mirror=False,
                 )
 
             detection_pipeline = INFERENCE_PIPELINE(
